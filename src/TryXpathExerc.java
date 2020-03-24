7f63c95f001cac62a6721dafa9bfcf56cc54a59a
mekgithub <mkortak1970@gmail.com>
2020-03-24 16:51:29 -0500

    4.st commit

--- src/TryXpathExerc.java
+++ src/TryXpathExerc.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class TryXpathExerc {
+
     public static void main(String[] args) throws InterruptedException {
         BrowserMain myBrowser=new BrowserMain();
         WebDriver driver=myBrowser.invokeChrome();
@@ -14,5 +15,8 @@ public class TryXpathExerc {
         Thread.sleep(2000);
         driver.findElement(By.xpath("//a[text()='Forgot account?']")).click();//this method for using text()..
        // driver.findElement(By.xpath("//input[@id='u_0_b']")).click();
+        String currentPage=driver.getCurrentUrl();
+        System.out.println("The current page is :"+currentPage);
+
     }
 }

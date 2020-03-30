c751b1b349b5899e08c7008dddf15b94469bd827
mekgithub <mkortak1970@gmail.com>
2020-03-30 17:45:55 -0500

    6.st commit

--- src/AExercises.java
+++ src/AExercises.java
@@ -1,16 +1,29 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AExercises {
    public static String url="http://www.amazon.com";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\18327\\Downloads\\chromedriver_win32_ version80\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("http://www.amazon.com");
-        driver.get("http://www.facebook.com");
-
-        driver.navigate().back();
+       // driver.get("http://www.facebook.com");
+     //   driver.navigate().back();
+        Thread.sleep(5000);
+        System.out.println("1");
+        driver.manage().window().maximize();
+        //driver.findElement(By.xpath("//div[@id='nav-xshop']/a[6]")).click();
+        driver.findElement(By.cssSelector("#nav-xshop > a:nth-child(6)")).click();//for css same part
+        Thread.sleep(5000);
+        System.out.println("2");
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("flour");
+        System.out.println("3");
+        //Thread.sleep(5000);
+        driver.findElement(By.cssSelector("input.nav-input")).click();
+        Thread.sleep(2000);
+        //driver.close();
 
     }
     //OUTPUT = Current url is: https://www.amazon.com/
--- src/CssSelectorId.java
+++ src/CssSelectorId.java
@@ -1,5 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
 public class CssSelectorId {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        BrowserMain myBrowser=new BrowserMain();
+        WebDriver driver=myBrowser.invokeChrome();
+        driver.get("https://www.expedia.com");
+        driver.findElement(By.cssSelector("input#flight-origin-hp-flight")).clear();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("input#flight-origin-hp-flight")).sendKeys("Atl");//it is not mandatory too use input
+        Thread.sleep(1000);
+        System.out.println("atlanata");
+        for( int i=0;i<4;i++){
+            driver.findElement(By.cssSelector("#flight-origin-hp-flight")).sendKeys(Keys.ARROW_DOWN);//atlanatada 3 havalani var,
+            // 3.cusunu DeKalb i sececgimiz icin 4 kez mouse down yapmali.
+        }
+
 
     }
 }
